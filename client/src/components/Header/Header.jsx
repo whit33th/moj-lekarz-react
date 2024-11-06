d9df176194cfeb012fb652aebe5798816ec82bc8
Daniil Kaminskyi <98901710+whit33th@users.noreply.github.com>
2024-11-06 21:56:19 +0100

    fix  header link

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -22,63 +22,110 @@ function Header({ isLoggedIn }) {
       <div className={styles.headerTopBlock}>tryb dla słabowidzących</div>
       <div className={styles.headerBottomBlock}>
         <div className={styles.headerLogo}>
-          <NavLink to="/"><img src={logo} alt="Logo" /></NavLink>
+          <NavLink to="/">
+            <img src={logo} alt="Logo" />
+          </NavLink>
         </div>
-        <div className={`${styles.headerNavigate} ${mobileMenuState ? styles.activeMobileMenu : styles.hiddenMobileMenu}`}>
+        <div
+          className={`${styles.headerNavigate} ${
+            mobileMenuState ? styles.activeMobileMenu : styles.hiddenMobileMenu
+          }`}
+        >
           <ul>
-            <li className={cx(styles.headerNavigateItem, { [styles.active]: location.pathname === '/howitwork' })}>
-              <NavLink to="/howitwork">Jak to działa</NavLink>
+            <li
+              className={cx(styles.headerNavigateItem, {
+                [styles.active]: location.pathname === "/how-it-work",
+              })}
+            >
+              <NavLink to="/how-it-work">Jak to działa</NavLink>
             </li>
-            <li className={cx(styles.headerNavigateItem, { [styles.active]: location.pathname === '/znajdz-lekarza' })}>
+            <li
+              className={cx(styles.headerNavigateItem, {
+                [styles.active]: location.pathname === "/znajdz-lekarza",
+              })}
+            >
               <NavLink to="/znajdz-lekarza">Znajdż lekarza</NavLink>
             </li>
-            {
-              isLoggedIn && <li className={cx(styles.headerNavigateItem, { [styles.active]: location.pathname === '/visits' })}>
+            {isLoggedIn && (
+              <li
+                className={cx(styles.headerNavigateItem, {
+                  [styles.active]: location.pathname === "/visits",
+                })}
+              >
                 <NavLink to="/visits">Moje wizyty</NavLink>
               </li>
-
-            }
-            {
-              isLoggedIn && <li className={cx(styles.headerNavigateItem, { [styles.active]: location.pathname === '/recipes' })}>
+            )}
+            {isLoggedIn && (
+              <li
+                className={cx(styles.headerNavigateItem, {
+                  [styles.active]: location.pathname === "/recipes",
+                })}
+              >
                 <NavLink to="/recipes">Moje recepty</NavLink>
               </li>
-
-            }
-            {
-              isLoggedIn && <li className={cx(styles.headerNavigateItem, { [styles.active]: location.pathname === '/ResearchResult' })}>
+            )}
+            {isLoggedIn && (
+              <li
+                className={cx(styles.headerNavigateItem, {
+                  [styles.active]: location.pathname === "/ResearchResult",
+                })}
+              >
                 <NavLink to="/ResearchResult">Dokumenty</NavLink>
               </li>
-
-            }
-            <li className={cx(styles.headerNavigateItem, { [styles.active]: location.pathname === '/mobilna' })}>
+            )}
+            <li
+              className={cx(styles.headerNavigateItem, {
+                [styles.active]: location.pathname === "/mobilna",
+              })}
+            >
               <NavLink to="/mobilna">Aplikacja mobilna</NavLink>
             </li>
-            <li className={cx(styles.headerNavigateItem, { [styles.active]: location.pathname === '/blog' })}>
+            <li
+              className={cx(styles.headerNavigateItem, {
+                [styles.active]: location.pathname === "/blog",
+              })}
+            >
               <NavLink to="/blog">Blog</NavLink>
             </li>
-            {
-              !isLoggedIn && <li className={cx(styles.headerNavigateItem, { [styles.active]: location.pathname === '/firm' })}>
+            {!isLoggedIn && (
+              <li
+                className={cx(styles.headerNavigateItem, {
+                  [styles.active]: location.pathname === "/firm",
+                })}
+              >
                 <NavLink to="/firm">Dla firm</NavLink>
               </li>
-            }
-            {
-              isLoggedIn
-                ? <li className={styles.headerProfileBtnMobile}><NavLink to="/Profile">Profile</NavLink></li>
-                : <li className={styles.headerSignBtnMobile}><NavLink to="/Auth">Zaloguj się</NavLink></li>
-            }
+            )}
+            {isLoggedIn ? (
+              <li className={styles.headerProfileBtnMobile}>
+                <NavLink to="/Profile">Profile</NavLink>
+              </li>
+            ) : (
+              <li className={styles.headerSignBtnMobile}>
+                <NavLink to="/Auth">Zaloguj się</NavLink>
+              </li>
+            )}
           </ul>
         </div>
-        {
-          isLoggedIn
-            ? <div className={styles.headerProfileBtn}><NavLink to="/Profile"><img src={imgProfile} alt="profile" /></NavLink></div>
-            : <div className={styles.headerSignBtn}><NavLink to="/Auth">Zaloguj się</NavLink></div>
-        }
-        <div className={styles.headerMobileBtn} onClick={() => setMobileMenuState(!mobileMenuState)}>
+        {isLoggedIn ? (
+          <div className={styles.headerProfileBtn}>
+            <NavLink to="/Profile">
+              <img src={imgProfile} alt="profile" />
+            </NavLink>
+          </div>
+        ) : (
+          <div className={styles.headerSignBtn}>
+            <NavLink to="/Auth">Zaloguj się</NavLink>
+          </div>
+        )}
+        <div
+          className={styles.headerMobileBtn}
+          onClick={() => setMobileMenuState(!mobileMenuState)}
+        >
           <span></span>
           <span></span>
           <span></span>
         </div>
-
       </div>
     </header>
   );
