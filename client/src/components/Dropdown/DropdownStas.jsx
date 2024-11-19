9339945b8aad2d6ad6e6e147a7ec4d1e8c5bd6f8
vilka13 <lukumer1997@gmail.com>
2024-11-19 08:45:08 +0100

    19.11.24-dropdown

--- client/src/components/Dropdown/DropdownStas.jsx
+++ client/src/components/Dropdown/DropdownStas.jsx
@@ -1,12 +1,34 @@
 import { useState, useRef, useEffect } from "react";
 import styles from "./DropdownStas.module.css";
+import unpointed from "../../assets/img/unpointed.png";
+import pointed from "../../assets/img/pointed.png";
+import Filter from "../Modals/Filter/Filter";
 
-const DropdownStas = ({ options, label, placeholder, onChange }) => {
+const DropdownStas = ({
+                        label,
+                        options = ["No options"],
+                        color,
+                        defaultOption = "Wybierz...",
+                        children,
+                        childrenLeft,
+                        selectedOptionChanging = true,
+                        listStyle,
+                        type, // Тип выпадающего меню, например, фильтр
+                        onChange,
+                      }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState(
-    placeholder || options[0] || "Wybierz"
+      defaultOption || options[0]
   );
   const dropdownRef = useRef(null);
+
+  // Состояние фильтра
+  const [filterState, setFilterState] = useState({
+    selectedGender: "K",
+    selectedSortOption: "az",
+    dateFrom: "",
+    dateTo: "",
+  });
 
   const toggleDropdown = () => {
     setIsOpen((prev) => !prev);
@@ -14,59 +36,84 @@ const DropdownStas = ({ options, label, placeholder, onChange }) => {
 
   const handleOptionClick = (option) => {
     setSelectedOption(option);
-    setIsOpen(false);
     if (onChange) {
-      onChange(option); 
+      onChange(option);
     }
+  };
+
+  const handleFilterChange = (newFilterState) => {
+    setFilterState(newFilterState);
+  };
+
+  const resetFilter = () => {
+    setFilterState({
+      selectedGender: "K",
+      selectedSortOption: "az",
+      dateFrom: "",
+      dateTo: "",
+    });
   };
 
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setIsOpen(false); 
+        setIsOpen(false);
       }
     };
 
     document.addEventListener("mousedown", handleClickOutside);
-
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
   return (
-    <div ref={dropdownRef} className={styles.dropdownContainer}>
-      {label && <label>{label}</label>}
-      <div
-        className={`${styles.dropdown} ${isOpen ? styles.open : ""}`}
-        onClick={toggleDropdown}
-      >
-        <span
-          className={selectedOption === placeholder ? styles.placeholder : ""}
+      <div ref={dropdownRef} className={styles.dropdownContainer}>
+        {label && <label>{label}</label>}
+        <div
+            style={{ backgroundColor: color }}
+            className={`${styles.dropdown} ${isOpen ? styles.open : ""}`}
+            onClick={toggleDropdown}
         >
-          {selectedOption}
-        </span>
-        {options && (
-          <span
-            className={`${styles.arrow} ${isOpen ? styles.arrowOpen : ""}`}
-          ></span>
-        )}
+          {childrenLeft}
+          {selectedOptionChanging === false
+              ? defaultOption
+              : selectedOption || "Wybierz..."}
+          {children}
+
+          {isOpen &&
+              (type === "filter" ? (
+                  <Filter
+                      onClick={(e) => e.stopPropagation()}
+                      filterState={filterState}
+                      onFilterChange={handleFilterChange}
+                      onReset={resetFilter}
+                  />
+              ) : (
+                  <ul className={styles.dropdownMenu}>
+                    {options.map((option) => (
+                        <li
+                            key={option}
+                            className={styles.dropdownMenuItem}
+                            onClick={() => handleOptionClick(option)}
+                        >
+                          <div className={styles.center}>
+                            {listStyle === "elipse" ? (
+                                <img
+                                    src={selectedOption !== option ? unpointed : pointed}
+                                    alt=""
+                                />
+                            ) : (
+                                ""
+                            )}
+                            <span className={styles.option}>{option}</span>
+                          </div>
+                        </li>
+                    ))}
+                  </ul>
+              ))}
+        </div>
       </div>
-      {isOpen && (
-        <ul className={styles.dropdownMenu}>
-          {options &&
-            options.map((option, index) => (
-              <li
-                key={index}
-                className={styles.dropdownMenuItem}
-                onClick={() => handleOptionClick(option)}
-              >
-                {option}
-              </li>
-            ))}
-        </ul>
-      )}
-    </div>
   );
 };
 
